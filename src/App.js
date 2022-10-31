16cfed96bdda9a10f3b2b8b1927f2a6f9f269c67
Ayumas Qonita <80805626+AdzS72@users.noreply.github.com>
2022-10-31 22:55:37 +0700

    Modul 13

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./Header"; //Include Heder
+import Footer from "./Footer"; //Include Footer
+import Body from "./Body"; //Include Footer
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="maincontainer">
+        <Header></Header>
+        <Body></Body>
+        <Footer></Footer>
+      </div>
+    );
+  }
 }
-
 export default App;
